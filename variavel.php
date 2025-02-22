7dd54747163e5d8cb4ebb0e9940d6fe8cdacab68
alunoEtec <aula@etec.com>
2025-02-21 22:49:14 -0300

    aula 21/02

--- variavel.php
+++ variavel.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>entre em contato</title>
+    <title>Document</title>
 </head>
 <body>
-<a href= "index.php"> Página inicial </a>
-    <?php 
-    echo "<h1>pagina de contato do site</h1>"
-    ?>
+    <h1>pagina das variaveis</h1>
+    <img src="./img/sk.jpg" alt="Bootstrap" width="30" height="24">
 </body>
 </html>
+
